6cf7aaa6820a5aa10f02738421dee0edcbbcb995
Brad <brad.nguyen2512@gmail.com>
2022-07-08 12:06:43 +0700

    7/8 Update 4: Deletion of tempCodeRunnerFile

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-# Writing To Files
-# ''''a' stands for append'''
-# employee_file = open('Employees.txt', 'w')
-# employee_file.write('Jim - Salesman\nDwight - Salesman\nPam - Receptionist\nMichael - Manager\nOscar - Accountant\nTobey - Human Resources\nKelley - Customer Service')
-# employee_file.close()
