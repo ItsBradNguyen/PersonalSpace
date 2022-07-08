426b87ef3559a6c4ed5ebb061739c49149a134e4
Brad <brad.nguyen2512@gmail.com>
2022-07-08 13:31:53 +0700

    7/9 Update 6: PythonIntermediate

---
+++ PythonIntermediate.py
@@ -0,0 +1,12 @@
+# # Lists:
+# my_items = ['apple','banana','coconut']
+# my_items_copied = my_items[:]
+# my_items_copied.append('dragonfruit',)
+# print(my_items)
+# print(my_items_copied)
+
+# # Tuples
+# ''' What separates Tuples from Lists is () and []'''
+# my_tuples = tuple(['Max',28, 'Boston'])
+
+# print(my_tuples)
