53f89c9eb8e9bc9fedaed2ec0eee223ed53a342d
Brad <brad.nguyen2512@gmail.com>
2022-07-08 19:40:49 +0700

    7/8 Update 8: PythonIntermediate

--- PythonIntermediate.py
+++ PythonIntermediate.py
@@ -1,4 +1,4 @@
-# # Lists:
+# # Lists
 # my_items = ['apple','banana','coconut']
 # my_items_copied = my_items[:]
 # my_items_copied.append('dragonfruit',)
@@ -10,3 +10,20 @@
 # my_tuples = tuple(['Max',28, 'Boston'])
 
 # print(my_tuples)
+
+# Set
+fruit = {'apple','banana','coconut'}
+print(fruit)
+
+for object in fruit:
+    print(object)
+
+fruit.add('cherry')
+fruit.add('dragonfruit')
+
+fruit2 = {'eggplant','fascell mango','grape'}
+fruit.update(fruit2)
+print(fruit)
+
+fruit.remove('coconut')
+print(fruit)
