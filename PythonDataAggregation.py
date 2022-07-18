6096e83e563b7d21808fd575d997fbf3d27d08ab
Brad <brad.nguyen2512@gmail.com>
2022-07-18 18:05:51 +0700

    7/18: Update 1 - Python Data Aggregation

--- PythonDataAggregation.py
+++ PythonDataAggregation.py
@@ -3,9 +3,3 @@ with open('departments.csv', newline='') as csvfile:
     data = csv.reader(csvfile,delimiter=' ', quotechar='|')
     for row in data:
         print(' '.join(row))
-
-import matplotlib.pyplot as plt
-plt.plot(csvfile)
-plt.show()
-
-
